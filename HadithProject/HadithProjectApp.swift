8420f03415e4da36ef09fa6a64af32a1cbe5512e
AsumahBanda <117347715+AsumahBanda@users.noreply.github.com>
2024-02-21 10:15:00 +0000

    Initial Commit

---
+++ HadithProject/HadithProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HadithProjectApp.swift
+//  HadithProject
+//
+//  Created by AMALITECH-PC-593 on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct HadithProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
